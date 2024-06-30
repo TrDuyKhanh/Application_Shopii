ad236d233bea3f8c939df82dfcd787a90a7b36b4
TrDuyKhanh <mailkhanh02@gmail.com>
2024-06-30 15:59:06 +0700

    add firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.androidApplication)
+
 }
 
 android {
@@ -40,4 +41,18 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+    implementation("com.google.firebase:firebase-analytics")
+
+
+    //firestore
+    implementation("com.google.firebase:firebase-firestore")
+    //authentication
+    implementation("com.google.firebase:firebase-auth")
+    //storage
+    implementation("com.google.firebase:firebase-storage")
+    //glide
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
 }
--- app/src/main/java/com/example/application_shopii/MainActivity.java
+++ app/src/main/java/com/example/application_shopii/MainActivity.java
@@ -8,7 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
 }
